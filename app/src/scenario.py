a9099202fe34786d73333194065c2c8d0ed98000
Fr0stFree <me@frostfree.ru>
2023-05-24 12:31:19 +0300

    add linters

--- app/src/scenario.py
+++ app/src/scenario.py
@@ -1,12 +1,13 @@
 import datetime as dt
 from pathlib import Path
-from typing import Type, Self
+from typing import Type
+from typing_extensions import Self
 
 from selenium.webdriver.common.by import By
 
 from .webdriver.interface import DriverInterface, Locator
 from .exceptions import ScenarioFailed
-from .settings import base_logger, error_logger
+from .settings import base_logger
 
 
 class RP5PageLocators:
@@ -48,45 +49,45 @@ class RP5ParseScenario:
             raise ScenarioFailed(f'Unable to stop webdriver: {self._Driver.__name__}.\n'
                                  f'An error occurred: {exc}')
 
-    def _goto_page(self, url: str) -> Self:
+    def goto_page(self, url: str) -> Self:
         base_logger.debug(f'Opening "{url}"...')
         self._driver_instance.open(url)
         return self
 
-    def _goto_archive_tab(self) -> Self:
-        base_logger.debug(f'Going to archive tab...')
+    def goto_archive_tab(self) -> Self:
+        base_logger.debug('Going to archive tab...')
         self._driver_instance.click(RP5PageLocators.ARCHIVE_DOWNLOAD_TAB)
         return self
 
-    def _select_csv_format(self) -> Self:
-        base_logger.debug(f'Selecting csv format...')
+    def select_csv_format(self) -> Self:
+        base_logger.debug('Selecting csv format...')
         self._driver_instance.click(RP5PageLocators.CSV_FORMAT_RADIO_BUTTON)
         return self
 
-    def _select_utf8_encoding(self) -> Self:
-        base_logger.debug(f'Selecting utf-8 encoding...')
+    def select_utf8_encoding(self) -> Self:
+        base_logger.debug('Selecting utf-8 encoding...')
         self._driver_instance.click(RP5PageLocators.UTF8_ENCODING_RADIO_BUTTON)
         return self
 
-    def _enter_min_calendar_date(self) -> Self:
+    def enter_min_calendar_date(self) -> Self:
         base_logger.debug(f'Setting up min calendar date to {self._min_date}...')
         self._driver_instance.input(RP5PageLocators.START_DATE_INPUT,
                                     value=self._min_date.strftime(self.CALENDAR_DATE_FORMAT))
         return self
 
-    def _enter_max_calendar_date(self) -> Self:
+    def enter_max_calendar_date(self) -> Self:
         base_logger.debug(f'Setting up max calendar date to {self._max_date}...')
         self._driver_instance.input(RP5PageLocators.START_DATE_INPUT,
                                     value=self._max_date.strftime(self.CALENDAR_DATE_FORMAT))
         return self
 
-    def _request_archive(self) -> Self:
-        base_logger.debug(f'Requesting archive...')
+    def request_archive(self) -> Self:
+        base_logger.debug('Requesting archive...')
         self._driver_instance.click(RP5PageLocators.REQUEST_ARCHIVE_BUTTON)
         return self
 
-    def _download_archive(self, save_to: Path) -> Self:
-        base_logger.debug(f'Downloading archive...')
+    def download_archive(self, save_to: Path) -> Self:
+        base_logger.debug('Downloading archive...')
         self._driver_instance.download(RP5PageLocators.DOWNLOAD_ARCHIVE_BUTTON, save_to)
         return self
 
@@ -96,14 +97,14 @@ class RP5ParseScenario:
             raise AttributeError('Driver has not been started.')
 
         try:
-            self._goto_page(url) \
-                ._goto_archive_tab() \
-                ._select_csv_format() \
-                ._select_utf8_encoding() \
-                ._enter_min_calendar_date() \
-                ._enter_max_calendar_date() \
-                ._request_archive() \
-                ._download_archive(save_to)
+            self.goto_page(url) \
+                .goto_archive_tab() \
+                .select_csv_format() \
+                .select_utf8_encoding() \
+                .enter_min_calendar_date() \
+                .enter_max_calendar_date() \
+                .request_archive() \
+                .download_archive(save_to)
             base_logger.info(f'Data from "{url}" has been parsed successfully')
             return save_to
         except Exception as exc:
